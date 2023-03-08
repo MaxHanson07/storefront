3f1ea16811c7840fd01b8924743a2b51eb069ee9
Max Hanson <mhanson19@georgefox.edu>
2023-03-07 20:20:55 -0800

    added to shopping cart model

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 from . import views
 
-router = SimpleRouter()
-router.register('products', views.ProductViewSet)
+
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
 
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
 # URL Configuration
-urlpatterns = router.urls
+urlpatterns = router.urls + products_router.urls
--- store/views.py
+++ store/views.py
@@ -1,13 +1,12 @@
+# from store.filters import ProductFilter
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
-from django.http import HttpResponse
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework.decorators import api_view
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
+from .pagination import DefaultPagination
 from .models import Collection, OrderItem, Product, Review
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
@@ -15,6 +14,11 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['collection_id']
+    pagination_class = DefaultPagination
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -38,5 +42,10 @@ class CollectionViewSet(ModelViewSet):
         return super().destroy(request, *args, **kwargs)
     
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
